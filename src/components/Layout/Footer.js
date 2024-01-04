d25969c56f1b2c9d1ac2fa518f9410c5f024662f
imamulahsan <imamulahsan@hotmail.com>
2024-01-04 20:40:41 +0100

    completed footer

--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -3,74 +3,80 @@ import InstagramIcon from "@mui/icons-material/Instagram";
 import TwitterIcon from "@mui/icons-material/Twitter";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import YouTubeIcon from "@mui/icons-material/YouTube";
-import { Box, Typography, Link } from "@mui/material";
+import { Box, Link } from "@mui/material";
 import footerImage from "../../images/footer-logo.png";
 
 const Footer = () => {
   return (
     <>
       <Box
-        sx={{ textAlign: "center", bgcolor: "#1A1A19", color: "white", p: 3 }}
+        sx={{
+          display: "flex",
+          flexDirection: "row", // Set flexDirection to "row"
+          alignItems: "center",
+          justifyContent: "space-between",
+          bgcolor: "#000000",
+          color: "white",
+          padding: "0 13%",
+        }}
       >
-        <Box
-          sx={{
-            display: "flex",
-            justifyContent: "space-between",
-            alignItems: "center",
-          }}
-        >
-          {/* Left side with image and links */}
-          <Box>
-            <Link href="/home" color="inherit" underline="none">
-              <img
-                src={footerImage}
-                alt="Your Footer"
-                style={{ height: "60px", marginRight: "10px" }}
-              />
-            </Link>
-            {/* Add more links as needed */}
-            <Link href="/about" color="inherit" underline="none">
-              About Us
-            </Link>
-            <Link href="/contact" color="inherit" underline="none">
-              Contact Us
-            </Link>
-          </Box>
+        {/* Left side with image */}
+        <img
+          src={footerImage}
+          alt="Your Footer"
+          style={{ height: "150px", width: "150px", margin: "10px" }}
+        />
 
-          {/* Right side with social icons */}
-          <Box
-            sx={{
-              "& svg": {
-                fontSize: "60px",
-                cursor: "pointer",
-                mr: 2,
-              },
-              "& svg:hover": {
-                color: "goldenrod",
-                transform: "translateX(5px)",
-                transition: "all 400ms",
-              },
-            }}
+        {/* Middle side with links */}
+        <Box>
+          {/* Add more links as needed */}
+          <Link
+            href="/home"
+            color="inherit"
+            underline="none"
+            sx={{ marginBottom: "5px" }}
           >
-            {/* icons */}
-            <InstagramIcon />
-            <TwitterIcon />
-            <GitHubIcon />
-            <YouTubeIcon />
-          </Box>
+            Home
+          </Link>
+          <Link
+            href="/about"
+            color="inherit"
+            underline="none"
+            sx={{ display: "block", marginBottom: "5px" }}
+          >
+            About Us
+          </Link>
+          <Link
+            href="/contact"
+            color="inherit"
+            underline="none"
+            sx={{ display: "block", marginBottom: "5px" }}
+          >
+            Contact Us
+          </Link>
         </Box>
 
-        <Typography
-          variant="h5"
+        {/* Right side with social icons */}
+        <Box
           sx={{
-            marginTop: 3,
-            "@media (max-width:600px)": {
-              fontSize: "1rem",
+            "& svg": {
+              fontSize: "60px",
+              cursor: "pointer",
+              mb: 1, // Adjust margin-bottom for social icons
+            },
+            "& svg:hover": {
+              color: "goldenrod",
+              transform: "translateX(5px)",
+              transition: "all 400ms",
             },
           }}
         >
-          All Rights Reserved &copy; Imamul Ahsan
-        </Typography>
+          {/* icons */}
+          <InstagramIcon />
+          <TwitterIcon />
+          <GitHubIcon />
+          <YouTubeIcon />
+        </Box>
       </Box>
     </>
   );
