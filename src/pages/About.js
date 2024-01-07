1f87ab085d40363794210ffce9a18d5c9c1f9cbd
imamulahsan <imamulahsan@hotmail.com>
2024-01-07 20:01:13 +0100

    started about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,34 @@
 import React from "react";
+import Layout from "../components/Layout/Layout";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <Layout>
+      <div
+        className="two-column-container"
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        {/* Column 1: Image */}
+        <div className="content-column" style={{ textAlign: "center" }}>
+          {/* You can add additional content or styling for the image column here */}
+          <h3 style={{ color: "#000000" }}>
+            Vettersstraße 54/110, 09126, Berlin
+          </h3>
+        </div>
+
+        {/* Column 2: Location Content */}
+        <div className="content-column">
+          <div className="headerContainer">
+            <h1>hello</h1>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
 };
 
 export default About;
